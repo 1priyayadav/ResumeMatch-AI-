67b4f5ff33815c3ce284d8abccd0e4cf547eb3e9
1priyayadav <130156879+1priyayadav@users.noreply.github.com>
2026-03-13 15:55:01 +0530

    Delete JobFit-Analyzer-main/client directory

--- JobFit-Analyzer-main/client/src/utils/ProtectedRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ checkIsLoggedIn }) => {
-  return checkIsLoggedIn ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
